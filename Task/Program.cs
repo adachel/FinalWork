1834d5e5efec12b54ffe066066bd9c40a47cca69
Dmitry <adachel74@gmail.com>
2023-02-27 13:44:58 +0500

    Task вводные данные

---
+++ Task/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длинна которых меньше либо равна 3. 
+
+Console.Clear();
+Console.WriteLine("Введите данные через пробел: ");
+string[] inarr = Console.ReadLine()!.Split(' ');
+string[] outarr = new string[inarr.Length];
+
