e22a1725109689e3f1a7c7df6c7b377e7866343f
Dmitry <adachel74@gmail.com>
2023-02-27 14:00:42 +0500

    Task вывод

--- Task/Program.cs
+++ Task/Program.cs
@@ -13,9 +13,20 @@ void Calc(string[] inarr, string[] outarr)
         }
     }
 }
+
+string Print(string[] text)
+{
+    string count = string.Empty;
+    for(int i = 0; i < text.Length; i++)
+        count = count + $" {text[i]}";
+    return count;    
+}
+
 Console.Clear();
 Console.WriteLine("Введите данные через пробел: ");
 string[] inarr = Console.ReadLine()!.Split(' ');
 string[] outarr = new string[inarr.Length];
 Calc(inarr, outarr);
+Console.WriteLine();
+Console.Write($"{Print(outarr)}");
 
